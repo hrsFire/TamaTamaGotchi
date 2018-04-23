ff7053b4ffb8a9782a84f7632eb98ed9436df7d7
Helmut Rhomberg <helmut_rhomberg@gmx.at>
2018-04-23 12:15:57 +0200

    added changes for the fragmentations (they use now contracts)

--- app/src/main/java/at/teamgotcha/tamagotchi/fragments/SettingsFragment.java
+++ app/src/main/java/at/teamgotcha/tamagotchi/fragments/SettingsFragment.java
@@ -2,27 +2,32 @@ package at.teamgotcha.tamagotchi.fragments;
 
 import android.content.Context;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import com.beardedhen.androidbootstrap.BootstrapButton;
 import at.teamgotcha.tamagotchi.R;
+import at.teamgotcha.tamagotchi.base.ContractV4Fragment;
+import at.teamgotcha.tamagotchi.interfaces.SettingsContractable;
 
-public class SettingsFragment extends Fragment {
+public class SettingsFragment extends ContractV4Fragment<SettingsContractable> {
     private BootstrapButton restartButton;
     private BootstrapButton languageButton;
     private BootstrapButton volumeButton;
     private BootstrapButton notificationButton;
     private BootstrapButton selectPetButton;
-    // private OnFragmentInteractionListener mListener;
 
     public SettingsFragment() {
+    }
+
+    public void onAttach(Context context) {
+        super.onAttach(context);
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         if (getArguments() != null) {
             //mParam2 = getArguments().getString(ARG_PARAM2);
         }
@@ -49,53 +54,39 @@ public class SettingsFragment extends Fragment {
         super.onResume();
     }
 
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-
-        // https://developer.android.com/training/basics/fragments/communicating.html
-        /*if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }*/
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        // mListener = null;
-    }
-
     private void setListeners() {
         restartButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                getContract().showRestartDialog();
             }
         });
 
         languageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                getContract().showLanguageDialog();
             }
         });
 
         volumeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                getContract().showVolumeDialog();
             }
         });
 
         notificationButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                getContract().showNotificationDialog();
             }
         });
 
         selectPetButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                getContract().showSelectPetDialog();
             }
         });
     }
---
+++ app/src/main/java/at/teamgotcha/tamagotchi/interfaces/SettingsContractable.java
@@ -0,0 +1,9 @@
+package at.teamgotcha.tamagotchi.interfaces;
+
+public interface SettingsContractable {
+    void showRestartDialog();
+    void showLanguageDialog();
+    void showVolumeDialog();
+    void showNotificationDialog();
+    void showSelectPetDialog();
+}
