7037741bd4a185152330b79d1332ea642971f3b6
Severin <severin1992@gmx.ch>
2018-04-23 10:48:17 +0200

    Working Notifications!

--- app/src/main/java/at/teamgotcha/helpers/NotificationHelper.java
+++ app/src/main/java/at/teamgotcha/helpers/NotificationHelper.java
@@ -3,50 +3,67 @@ package at.teamgotcha.helpers;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.support.v4.app.NotificationCompat;
 
 import at.teamgotcha.pets.Pet;
+import at.teamgotcha.tamagotchi.MainActivity;
 import at.teamgotcha.tamagotchi.R;
 
 public class NotificationHelper {
 
     private final static int REQ_CODE_HUNGER = 1;
 
+    @SuppressWarnings("deprecation")
     public static void addPetHungerNotification(Pet pet){
 
-        // Build the Notifcation (if required)
-        if(pet.isHungry() != null){
+        try {
+            // Build the Notifcation (if required)
+            if(pet.isHungry() != null){
 
-            NotificationManager notificationManager = (NotificationManager) pet.getContext().getSystemService(Context.NOTIFICATION_SERVICE);
+                NotificationManager notificationManager = (NotificationManager) pet.getContext().getSystemService(Context.NOTIFICATION_SERVICE);
 
-            NotificationCompat.Builder builder = null;
+                NotificationCompat.Builder builder;
 
-            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
 
-                int importance = NotificationManager.IMPORTANCE_DEFAULT;
+                    int importance = NotificationManager.IMPORTANCE_DEFAULT;
 
-                NotificationChannel notificationChannel = new NotificationChannel("ID", "Name", importance);
+                    NotificationChannel notificationChannel = new NotificationChannel("ID", "Name", importance);
 
-                notificationManager.createNotificationChannel(notificationChannel);
+                    notificationManager.createNotificationChannel(notificationChannel);
 
-                builder = new NotificationCompat.Builder(pet.getContext().getApplicationContext(), notificationChannel.getId());
+                    builder = new NotificationCompat.Builder(pet.getContext().getApplicationContext(), notificationChannel.getId());
 
-            } else {
+                } else {
 
-                builder = new NotificationCompat.Builder(pet.getContext().getApplicationContext());
+                    // note: deprecation on purpose for backwards compatibility
+                    builder = new NotificationCompat.Builder(pet.getContext().getApplicationContext());
+                }
+
+                // Create an explicit intent for an Activity in your app
+                Intent intent = new Intent(pet.getContext(), MainActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                PendingIntent pendingIntent = PendingIntent.getActivity(pet.getContext(), 0, intent, 0);
+
+                builder = builder
+                        .setSmallIcon(R.drawable.icon_ball)
+                        // .setColor(ContextCompat.getColor(context, R.color.color))
+                        .setContentTitle(pet.getContext().getString(R.string.notification_hunger))
+                        // .setTicker(context.getString(R.string.text))
+                        .setContentText(pet.isHungry())
+                        .setDefaults(Notification.DEFAULT_ALL)
+                        .setContentIntent(pendingIntent)
+                        .setAutoCancel(true);
+
+                notificationManager.notify(REQ_CODE_HUNGER, builder.build());
             }
 
-            builder = builder
-                    .setSmallIcon(R.drawable.icon_ball)
-                    // .setColor(ContextCompat.getColor(context, R.color.color))
-                    .setContentTitle(pet.getContext().getString(R.string.notification_hunger))
-                    // .setTicker(context.getString(R.string.text))
-                    .setContentText(pet.isHungry())
-                    .setDefaults(Notification.DEFAULT_ALL)
-                    .setAutoCancel(true);
+        } catch (NullPointerException e){
 
-            notificationManager.notify(REQ_CODE_HUNGER, builder.build());
+            System.err.println(e.getMessage());
         }
 
     }
--- app/src/main/java/at/teamgotcha/tamagotchi/fragments/PetSpriteFragment.java
+++ app/src/main/java/at/teamgotcha/tamagotchi/fragments/PetSpriteFragment.java
@@ -1,6 +1,7 @@
 package at.teamgotcha.tamagotchi.fragments;
 
 import android.app.Fragment;
+import android.app.NotificationManager;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
@@ -8,6 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
+import at.teamgotcha.helpers.NotificationHelper;
 import at.teamgotcha.pets.Pet;
 import at.teamgotcha.pets.PetOne;
 import at.teamgotcha.tamagotchi.R;
@@ -30,6 +32,9 @@ public class PetSpriteFragment extends Fragment {
 
         currentPet = new PetOne(view);
         // updateSprite();
+
+        // Add Notification ...
+        NotificationHelper.addPetHungerNotification(currentPet);
 
         return view;
     }
