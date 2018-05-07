ae411c584e25221a3998fdb098a2420488470baf
nst5495 <nst5495@students.fhv.at>
2018-05-07 11:03:07 +0200

    Added persistence for Pet

---
+++ app/src/main/java/at/teamgotcha/tamagotchi/helpers/PetDB.java
@@ -0,0 +1,34 @@
+package at.teamgotcha.tamagotchi.helpers;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by nikol_000 on 07.05.2018.
+ */
+
+public class PetDB extends SQLiteOpenHelper{
+    private static final String DB_NAME = "tamatamagotchi.db";
+    private static final int VERSION = 1;
+
+    public PetDB(Context context) {
+        super(context,DB_NAME,null,VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL("CREATE TABLE pet(id INTEGER PRIMARY KEY AUTOINCREMENT," +
+                "health REAL NOT NULL," +
+                "mood REAL NOT NULL," +
+                "hunger REAL NOT NULL," +
+                "name TEXT NOT NULL," +
+                "gender INTEGER NOT NULL);");
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        sqLiteDatabase.execSQL("DROP TABLE pet");
+    }
+}
